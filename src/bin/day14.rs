1fb99ce388fdd443642de979c91bbf43a3200ab0
Adrian Perez de Castro <aperez@igalia.com>
2017-12-17 23:01:06 +0200

    Move more parts into shared code to reuse

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -6,39 +6,10 @@
 
 extern crate aoc2017;
 
-use aoc2017::day10::KnotHash;
+use aoc2017::day14::{ make_row_hash, hex_char_to_u8 };
 use std::io;
 use std::io::prelude::*;
 
-
-fn make_row_hash(key: &str, row: u16) -> String {
-    let input = format!("{}-{}", key, row);
-    let mut kh = KnotHash::new();
-    kh.rounds(input.as_bytes());
-    format!("{:x}", kh)
-}
-
-fn hex_char_to_u8(c: char) -> u8 {
-    match c {
-        '0' => 0x0,
-        '1' => 0x1,
-        '2' => 0x2,
-        '3' => 0x3,
-        '4' => 0x4,
-        '5' => 0x5,
-        '6' => 0x6,
-        '7' => 0x7,
-        '8' => 0x8,
-        '9' => 0x9,
-        'a' | 'A' => 0xA,
-        'b' | 'B' => 0xB,
-        'c' | 'C' => 0xC,
-        'd' | 'D' => 0xD,
-        'e' | 'E' => 0xE,
-        'f' | 'F' => 0xF,
-        _ => panic!("Non-hex character: '{}'", c),
-    }
-}
 
 fn hex_string_bits(s: &str) -> u32 {
     let mut bits = 0;
--- src/lib.rs
+++ src/lib.rs
@@ -184,3 +184,38 @@ pub mod day10
         }
     }
 }
+
+
+pub mod day14
+{
+    use super::day10::KnotHash;
+
+    pub fn make_row_hash(key: &str, row: u16) -> String {
+        let input = format!("{}-{}", key, row);
+        let mut kh = KnotHash::new();
+        kh.rounds(input.as_bytes());
+        format!("{:x}", kh)
+    }
+
+    pub fn hex_char_to_u8(c: char) -> u8 {
+        match c {
+            '0' => 0x0,
+            '1' => 0x1,
+            '2' => 0x2,
+            '3' => 0x3,
+            '4' => 0x4,
+            '5' => 0x5,
+            '6' => 0x6,
+            '7' => 0x7,
+            '8' => 0x8,
+            '9' => 0x9,
+            'a' | 'A' => 0xA,
+            'b' | 'B' => 0xB,
+            'c' | 'C' => 0xC,
+            'd' | 'D' => 0xD,
+            'e' | 'E' => 0xE,
+            'f' | 'F' => 0xF,
+            _ => panic!("Non-hex character: '{}'", c),
+        }
+    }
+}
