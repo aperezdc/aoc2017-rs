132f452efa866ccc6b0f4fcb4e82c9485c8c1fc3
Adrian Perez de Castro <aperez@igalia.com>
2017-12-17 22:56:52 +0200

    Move KnotHash to common code, to use it in solution for day 14

--- src/lib.rs
+++ src/lib.rs
@@ -110,3 +110,77 @@ pub mod day03 {
         return size;
     }
 }
+
+
+pub mod day10
+{
+    use std::fmt;
+
+    pub struct KnotHash
+    {
+        list: [u8; 256],
+        pos: usize,
+        skip: usize,
+    }
+
+    impl KnotHash
+    {
+        const ROUNDS: usize = 64;
+        const XORITEMS: usize = 16;
+        const XORGROUPS: usize = 256 / Self::XORITEMS;
+
+        pub fn new() -> Self {
+            let mut kh = KnotHash { list: [0u8; 256], pos: 0, skip: 0 };
+            for i in 0 .. kh.list.len() {
+                kh.list[i] = i as u8;
+            }
+            kh
+        }
+
+        fn apply(&mut self, n: u8) {
+            // 1. Reverse the order of elements pos..n
+            let len = self.list.len();
+            let mut i = self.pos;
+            let mut j = self.pos + (n as usize) - 1;
+            while i < j {
+                self.list.swap(i % len, j % len);
+                i += 1;
+                j -= 1;
+            }
+
+            // 2. Increase position by n+skip
+            self.pos = (self.pos + (n as usize) + self.skip) % self.list.len();
+
+            // 3. Increase skip by one.
+            self.skip += 1;
+        }
+
+        #[inline]
+        pub fn rounds(&mut self, input: &[u8]) {
+            for _ in 0..Self::ROUNDS {
+                for &b in input {
+                    self.apply(b);
+                }
+                // Apply the standard tail lengths.
+                for &b in &[17, 31, 73, 47, 23] {
+                    self.apply(b);
+                }
+            }
+        }
+    }
+
+    impl fmt::LowerHex for KnotHash
+    {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            for group in 0 .. Self::XORGROUPS {
+                let startpos = group * Self::XORITEMS;
+                let mut xor = 0;
+                for x in &self.list[startpos .. startpos + Self::XORITEMS] {
+                    xor ^= x;
+                }
+                write!(f, "{:02x}", xor)?;
+            }
+            Ok(())
+        }
+    }
+}
